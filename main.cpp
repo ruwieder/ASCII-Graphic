8d5276f1897193d1cb8634e39033019f5fc3ba8c
ruwieder <a@a.com>
2023-07-23 22:59:44 +0300

    moved functions to Console class

--- main.cpp
+++ main.cpp
@@ -3,45 +3,46 @@
 #define CONSOLE_HEIGHT 50
 #define CONSOLE_MODE_COMMAND "mode 150, 52"
 
-void update();
-void buffer_init();
+class Console{
+private:
+    uint8_t video_buffer[CONSOLE_WIDTH * CONSOLE_HEIGHT];
+public:
+    Console(){
+        buffer_init();
+    }
 
-uint8_t video_buffer[CONSOLE_WIDTH * CONSOLE_HEIGHT];
+    void buffer_init(){
+        for (size_t i = 0; i < CONSOLE_HEIGHT * CONSOLE_WIDTH; i++)
+            video_buffer[i] = 0;
+    }
+
+    void update(){
+        char symbol;
+        for (size_t i = 0; i < CONSOLE_HEIGHT; i++){
+            for (size_t j = 0; j < CONSOLE_WIDTH; j++){
+                switch (video_buffer[CONSOLE_WIDTH * i + j]){
+                case 0:
+                    symbol = ' ';
+                    break;
+                case 1:
+                    symbol = '#';
+                    break;
+                default:
+                    symbol = '?';
+                    break;
+                }
+                std::cout << symbol;
+            }
+            std::cout << std::endl;
+        }
+    }
+};
 
 int main(){
-    system(CONSOLE_MODE_COMMAND);
+    system(CONSOLE_MODE_COMMAND); // resize Windows console
 
-    buffer_init();
-    video_buffer[1024] = 1;
-    update();
+    Console console = Console();
+
+    console.update();
     system("pause");
-}
-
-void update(){
-    char symbol;
-    for (size_t i = 0; i < CONSOLE_HEIGHT; i++)
-    {
-        for (size_t j = 0; j < CONSOLE_WIDTH; j++)
-        {
-            switch (video_buffer[CONSOLE_WIDTH * i + j])
-            {
-            case 0:
-                symbol = ' ';
-                break;
-            case 1:
-                symbol = '#';
-                break;
-            default:
-                symbol = '?';
-                break;
-            }
-            std::cout << symbol;
-        }
-        std::cout << std::endl;
-    }
-}
-
-void buffer_init(){
-    for (size_t i = 0; i < CONSOLE_HEIGHT * CONSOLE_WIDTH; i++)
-        video_buffer[i] = 0;
 }
