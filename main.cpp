4c68e9edb001a0c0c188906e405ae60e9774bce1
ruwieder <a@a.com>
2023-07-29 23:14:48 +0300

    buffer containing digit values, not chars removed ellipse drawing

--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@
 #define CONSOLE_WIDTH 150
 #define CONSOLE_HEIGHT 50
 #define SYMBOL_CONST 3.5
+#define SYMBOLS_COUNT 8
 #define CONSOLE_MODE_COMMAND "mode 150, 52"
 
 class Console{
 private:
     uint8_t video_buffer[CONSOLE_WIDTH * CONSOLE_HEIGHT];
+    const char symbols[SYMBOLS_COUNT]{' ', '.', ',', ':', ';', 'x', 'X', '@'};
 public:
     Console(){
         buffer_init();
@@ -16,7 +18,7 @@ public:
 
     void buffer_init(){
         for (size_t i = 0; i < CONSOLE_HEIGHT * CONSOLE_WIDTH; i++)
-            video_buffer[i] = ' ';
+            video_buffer[i] = 0;
     }
 
     void update(){
@@ -27,20 +29,24 @@ public:
         }
     }
 
-    void set(uint16_t x, uint16_t y, uint8_t symbol){
-        video_buffer[y * CONSOLE_WIDTH + x] = symbol;
+    void set(uint16_t x, uint16_t y, uint8_t value){
+        video_buffer[y * CONSOLE_WIDTH + x] = value;
     }
 
-    void draw_ellipse(uint8_t x, uint8_t y, unsigned size, uint8_t symbol = '#', float ratio = 1){
-        for (uint8_t i = 0; i < CONSOLE_HEIGHT; i++)
-    {
-        for (uint8_t j = 0; j < CONSOLE_WIDTH; j++)
+    void clear_buffer(){
+        for (size_t i = 0; i < CONSOLE_HEIGHT * CONSOLE_WIDTH; i++)
         {
-            if (pow(abs(i - y), 2) + pow(abs(j - x), 2) / SYMBOL_CONST <= size * size){
-                    this->set(j, i, symbol);
-                }
+            this->video_buffer[i] = 0;
         }
     }
+
+    void draw_buffer(){
+        for (size_t i = 0; i < CONSOLE_HEIGHT; i++){
+            for (size_t j = 0; j < CONSOLE_WIDTH; j++)
+            {
+                std::cout << (symbols[video_buffer[i * CONSOLE_WIDTH + j] >> 5]);
+            }
+        }
     }
 };
 
@@ -48,8 +54,8 @@ int main(){
     system(CONSOLE_MODE_COMMAND); // resize Windows console
     Console console = Console();
 
-    console.draw_ellipse(75, 25, 10, '^');
-    console.update();
+    console.set(100, 43, 255);
+    console.draw_buffer();
 
     system("pause");
 }
