a6739f94fe282111b58fe67244244543a335ddbc
ruwieder <a@a.com>
2023-07-30 23:45:24 +0300

    move console resizing to function

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
+#include <string>
 #include "settings.cpp"
 #include "Console.cpp"
 #include "Coord.cpp"
 #include "Object.cpp"
-#define CONSOLE_MODE_COMMAND "mode 150, 52"
+
+void resize_console(){
+    std::string _width = std::to_string(CONSOLE_WIDTH);
+    std::string _height = std::to_string(CONSOLE_HEIGHT + 2);
+    std::string _command = "mode " + _width + ", " + _height;
+    system(_command.c_str());
+}
 
 using My::Console;
 
 int main(){
-    system(CONSOLE_MODE_COMMAND); // resize Windows console
+    resize_console();
+
     Console console = Console();
     for (size_t i = 20; i < 50; i++){
         console.set(i + 50, i, 180);
